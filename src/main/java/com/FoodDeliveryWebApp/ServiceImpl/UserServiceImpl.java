3a584644cbe3eff69b14c383ee9c7df6cbbbfcc3
Rajesh-patale <rajeshpatale888@gmail.com>
2024-07-16 14:27:33 +0530

    Added service annotation.

--- src/main/java/com/FoodDeliveryWebApp/ServiceImpl/UserServiceImpl.java
+++ src/main/java/com/FoodDeliveryWebApp/ServiceImpl/UserServiceImpl.java
@@ -1,11 +1,8 @@
 package com.FoodDeliveryWebApp.ServiceImpl;
 
-import com.FoodDeliveryWebApp.Entity.User;
-import com.FoodDeliveryWebApp.Repository.UserRepository;
-import com.FoodDeliveryWebApp.ServiceI.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserServiceImpl {
-
 
 }
