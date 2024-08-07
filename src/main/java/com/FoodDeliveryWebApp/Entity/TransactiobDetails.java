98fb8be6e50f633486ee6725e71cedfb9a513f4b
rakeshrajote <rakeshrajote2@gmail.com>
2024-08-07 15:14:30 +0530

    added time and date for payment

--- src/main/java/com/FoodDeliveryWebApp/Entity/TransactiobDetails.java
+++ src/main/java/com/FoodDeliveryWebApp/Entity/TransactiobDetails.java
@@ -1,58 +1,25 @@
 package com.FoodDeliveryWebApp.Entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import lombok.Getter;
-import lombok.Setter;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
 
 
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class TransactiobDetails {
-
 
 
     private String orderId;
     private String currency;
     private Integer amount;
     private String KEY;
+    private String createdAt; // New field for date and time
+    private String status;
 
-    public TransactiobDetails(String orderId, String currency, Integer amount , String KEY) {
-        this.orderId = orderId;
-        this.currency = currency;
-        this.amount = amount;
-        this.KEY = KEY;
-    }
 
-    public String getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(String orderId) {
-        this.orderId = orderId;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public Integer getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Integer amount) {
-        this.amount = amount;
-    }
-
-    public String getKEY() {
-        return KEY;
-    }
-
-    public void setKEY(String KEY) {
-        this.KEY = KEY;
-    }
 }
